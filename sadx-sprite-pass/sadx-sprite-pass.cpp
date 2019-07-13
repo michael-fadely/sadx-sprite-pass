8f82a2f8fe1e41af3f1f4c9c319a27779877cc8b
Michael Fadely <michaelwfadely@gmail.com>
2019-07-13 12:28:13 -0600

    implemented 3d sprite support

--- sadx-sprite-pass/sadx-sprite-pass.cpp
+++ sadx-sprite-pass/sadx-sprite-pass.cpp
@@ -1,5 +1,21 @@
 #include "pch.h"
 
+enum KMDEPTHMODE
+{
+	KM_IGNORE       = 0,
+	KM_LESS         = 1,
+	KM_EQUAL        = 2,
+	KM_LESSEQUAL    = 3,
+	KM_GREATER      = 4,
+	KM_NOTEQUAL     = 5,
+	KM_GREATEREQUAL = 6,
+	KM_ALWAYS       = 7
+};
+
+struct QueuedSprite;
+
+static std::vector<QueuedSprite> sprites_2d;
+static std::vector<QueuedSprite> sprites_3d;
 static std::vector<NJS_TEXANIM> texanims;
 static std::array<NJD_COLOR_BLENDING, 2> blending_modes{};
 
@@ -62,8 +78,6 @@ struct QueuedSprite
 	}
 };
 
-static std::vector<QueuedSprite> sprites;
-
 void __cdecl njColorBlendingMode__r(NJD_COLOR_TARGET target, NJD_COLOR_BLENDING mode);
 Trampoline njColorBlendingMode__t(0x0077EC60, 0x0077EC66, njColorBlendingMode__r);
 void __cdecl njColorBlendingMode__r(NJD_COLOR_TARGET target, NJD_COLOR_BLENDING mode)
@@ -92,15 +106,24 @@ void __cdecl njColorBlendingMode__r(NJD_COLOR_TARGET target, NJD_COLOR_BLENDING 
 	}
 }
 
-
-void enqueue_sprite(NJS_SPRITE* sp, Int n, Float pri, NJD_SPRITE attr)
+void enqueue_2d(NJS_SPRITE* sp, Int n, Float pri, NJD_SPRITE attr)
 {
 	if (!sp)
 	{
 		return;
 	}
 
-	sprites.emplace_back(sp, n, pri, attr);
+	sprites_2d.emplace_back(sp, n, pri, attr);
+}
+
+void enqueue_3d(NJS_SPRITE* sp, Int n, NJD_SPRITE attr)
+{
+	if (!sp)
+	{
+		return;
+	}
+
+	sprites_3d.emplace_back(sp, n, 0.0f, attr);
 }
 
 static void __cdecl njDrawSprite2D_DrawNow_r(NJS_SPRITE* sp, Int n, Float pri, NJD_SPRITE attr);
@@ -114,9 +137,11 @@ static void __cdecl njDrawSprite2D_DrawNow_o(NJS_SPRITE* sp, Int n, Float pri, N
 
 DataPointer(void*, Direct3D_Device, 0x03D128B0);
 
+// ReSharper disable once CppDeclaratorNeverUsed
 static void reset_stream_source()
 {
 	// all to avoid linking with d3d8.lib
+	// ReSharper disable once CppDeclaratorNeverUsed
 	const auto value = Direct3D_Device;
 
 	__asm
@@ -145,40 +170,7 @@ static void __cdecl njDrawSprite2D_DrawNow_r(NJS_SPRITE* sp, Int n, Float pri, N
 		return;
 	}
 
-	NJS_SPRITE _sp = *sp;
-	_sp.sx = 0.0f;
-	_sp.sy = 0.0f;
-
-	njDrawSprite2D_DrawNow_o(&_sp, n, pri, attr);
-
-	Direct3D_EnableHudAlpha((attr & NJD_SPRITE_ALPHA) == NJD_SPRITE_ALPHA);
-	enqueue_sprite(sp, n, pri, attr);
-	reset_stream_source();
-
-	Direct3D_SetTexList(sp->tlist);
-
-	NJS_COLOR color_, v16;
-
-	if (attr & NJD_SPRITE_COLOR)
-	{
-		color_.argb.b = (uint8_t)(_nj_constant_material_.b * 255.0f);
-		color_.argb.g = (uint8_t)(_nj_constant_material_.g * 255.0f);
-		color_.argb.r = (uint8_t)(_nj_constant_material_.r * 255.0f);
-		color_.argb.a = (uint8_t)(_nj_constant_material_.a * 255.0f);
-		v16.color     = color_.color;
-	}
-	else
-	{
-		v16.color = 0xFFFFFFFF;
-	}
-
-	Direct3D_EnableHudAlpha((attr & NJD_SPRITE_ALPHA) == NJD_SPRITE_ALPHA);
-
-	auto texid = sp->tanim[n].texid;
-
-	CurrentHUDColor = v16.color;
-	Direct3D_SetTextureNum(texid);
-	Direct3D_TextureFilterLinear();
+	enqueue_2d(sp, n, pri, attr);
 }
 
 //#define USE_QUEUED
@@ -196,11 +188,126 @@ static void __cdecl njDrawSprite2D_Queue_r(NJS_SPRITE* sp, Int n, Float pri, NJD
 
 	//NonStaticFunctionPointer(void, original, (NJS_SPRITE*, Int, Float, NJD_SPRITE, QueuedModelFlagsB), njDrawSprite2D_Queue_t.Target());
 	//original(sp, n, pri, attr, queue_flags);
-	enqueue_sprite(sp, n, pri, attr);
+	enqueue_2d(sp, n, pri, attr);
 }
 #endif
 
+static void __cdecl njDrawSprite3D_DrawNow_r(NJS_SPRITE* sp, int n, NJD_SPRITE attr);
+static Trampoline njDrawSprite3D_DrawNow_t(0x0077E390, 0x0077E398, &njDrawSprite3D_DrawNow_r);
+static void __cdecl njDrawSprite3D_DrawNow_r(NJS_SPRITE* sp, int n, NJD_SPRITE attr)
+{
+	if (!sp || !sp->tlist)
+	{
+		return;
+	}
+
+	enqueue_3d(sp, n, attr);
+}
+
+static void __cdecl njDrawSprite3D_DrawNow_o(NJS_SPRITE* sp, int n, NJD_SPRITE attr)
+{
+	auto original = reinterpret_cast<decltype(njDrawSprite3D_DrawNow_o)*>(njDrawSprite3D_DrawNow_t.Target());
+	original(sp, n, attr);
+}
+
 #define EXPORT __declspec(dllexport)
+
+HMODULE sadx_d3d11     = nullptr;
+void (*oit_enable_)()  = nullptr;
+void (*oit_disable_)() = nullptr;
+bool (*oit_enabled_)() = nullptr;
+
+void oit_enable()
+{
+	if (oit_enable_)
+	{
+		oit_enable_();
+	}
+}
+
+void oit_disable()
+{
+	if (oit_disable_)
+	{
+		oit_disable_();
+	}
+}
+
+bool oit_enabled()
+{
+	if (oit_enabled_)
+	{
+		return oit_enabled_();
+	}
+
+	return false;
+}
+
+void draw_sprites_2d()
+{
+	const bool is_oit_enabled = oit_enabled();
+	oit_disable();
+
+	Direct3D_EnableZWrite(false);
+	Direct3D_SetZFunc(KM_ALWAYS);
+	njColorBlendingMode_(NJD_SOURCE_COLOR, NJD_COLOR_BLENDING_SRCALPHA);
+	njColorBlendingMode_(NJD_DESTINATION_COLOR, NJD_COLOR_BLENDING_INVSRCALPHA);
+
+	njPushMatrix(nullptr);
+	for (auto& sprite : sprites_2d)
+	{
+		sprite.apply();
+		njDrawSprite2D_DrawNow_o(&sprite.sp, sprite.n, sprite.pri, sprite.attr);
+	}
+	njPopMatrix(1);
+
+	sprites_2d.clear();
+
+	Direct3D_EnableZWrite(false);
+	Direct3D_SetZFunc(KM_ALWAYS);
+	njColorBlendingMode_(NJD_SOURCE_COLOR, NJD_COLOR_BLENDING_SRCALPHA);
+	njColorBlendingMode_(NJD_DESTINATION_COLOR, NJD_COLOR_BLENDING_INVSRCALPHA);
+	ProbablyDrawDebugText(1);
+
+	if (is_oit_enabled)
+	{
+		oit_enable();
+	}
+
+	njColorBlendingMode_(NJD_SOURCE_COLOR, NJD_COLOR_BLENDING_SRCALPHA);
+	njColorBlendingMode_(NJD_DESTINATION_COLOR, NJD_COLOR_BLENDING_INVSRCALPHA);
+}
+
+void draw_sprites_3d()
+{
+	if (oit_enabled())
+	{
+		oit_enable();
+	}
+
+	Direct3D_EnableZWrite(false);
+	Direct3D_SetZFunc(KM_LESS);
+	njColorBlendingMode_(NJD_SOURCE_COLOR, NJD_COLOR_BLENDING_SRCALPHA);
+	njColorBlendingMode_(NJD_DESTINATION_COLOR, NJD_COLOR_BLENDING_INVSRCALPHA);
+
+	njPushMatrix(nullptr);
+	for (auto& sprite : sprites_3d)
+	{
+		sprite.apply();
+		njDrawSprite3D_DrawNow_o(&sprite.sp, sprite.n, sprite.attr);
+	}
+	njPopMatrix(1);
+
+	sprites_3d.clear();
+
+	Direct3D_EnableZWrite(false);
+	Direct3D_SetZFunc(KM_LESS);
+	njColorBlendingMode_(NJD_SOURCE_COLOR, NJD_COLOR_BLENDING_SRCALPHA);
+	njColorBlendingMode_(NJD_DESTINATION_COLOR, NJD_COLOR_BLENDING_INVSRCALPHA);
+
+	njColorBlendingMode_(NJD_SOURCE_COLOR, NJD_COLOR_BLENDING_SRCALPHA);
+	njColorBlendingMode_(NJD_DESTINATION_COLOR, NJD_COLOR_BLENDING_INVSRCALPHA);
+}
 
 extern "C"
 {
@@ -217,19 +324,16 @@ extern "C"
 		0
 	};
 
-	HMODULE sadx_d3d11 = nullptr;
-	void(*oit_enable_)() = nullptr;
-	void(*oit_disable_)() = nullptr;
-	bool(*oit_enabled_)() = nullptr;
-
 	EXPORT void Init()
 	{
-		sprites.reserve(512);
+		sprites_2d.reserve(512);
+		sprites_3d.reserve(1024);
 		texanims.reserve(128);
 
 		sadx_d3d11 = GetModuleHandle(L"sadx-d3d11.dll");
 
-		if (sadx_d3d11 != INVALID_HANDLE_VALUE)
+		if (sadx_d3d11 != INVALID_HANDLE_VALUE &&
+		    sadx_d3d11 != nullptr)
 		{
 			oit_enable_  = reinterpret_cast<decltype(oit_enable_)>(GetProcAddress(sadx_d3d11, "oit_enable"));
 			oit_disable_ = reinterpret_cast<decltype(oit_disable_)>(GetProcAddress(sadx_d3d11, "oit_disable"));
@@ -239,61 +343,9 @@ extern "C"
 		WriteData<5>((void*)0x0078B9E3, 0x90i8);
 	}
 
-	void oit_enable()
-	{
-		if (oit_enable_)
-		{
-			oit_enable_();
-		}
-	}
-
-	void oit_disable()
-	{
-		if (oit_disable_)
-		{
-			oit_disable_();
-		}
-	}
-
-	bool oit_enabled()
-	{
-		if (oit_enabled_)
-		{
-			return oit_enabled_();
-		}
-
-		return false;
-	}
-
 	EXPORT void OnRenderSceneEnd()
 	{
-		const bool is_oit_enabled = oit_enabled();
-		oit_disable();
-
-		Direct3D_EnableZWrite(false);
-		Direct3D_SetZFunc(7);
-		njColorBlendingMode_(NJD_SOURCE_COLOR, NJD_COLOR_BLENDING_SRCALPHA);
-		njColorBlendingMode_(NJD_DESTINATION_COLOR, NJD_COLOR_BLENDING_INVSRCALPHA);
-
-		njPushMatrix(nullptr);
-		for (auto& sprite : sprites)
-		{
-			sprite.apply();
-			njDrawSprite2D_DrawNow_o(&sprite.sp, sprite.n, sprite.pri, sprite.attr);
-		}
-		njPopMatrix(1);
-
-		sprites.clear();
-
-		Direct3D_EnableZWrite(false);
-		Direct3D_SetZFunc(7);
-		njColorBlendingMode_(NJD_SOURCE_COLOR, NJD_COLOR_BLENDING_SRCALPHA);
-		njColorBlendingMode_(NJD_DESTINATION_COLOR, NJD_COLOR_BLENDING_INVSRCALPHA);
-		ProbablyDrawDebugText(1);
-
-		if (is_oit_enabled)
-		{
-			oit_enable();
-		}
+		draw_sprites_3d();
+		draw_sprites_2d();
 	}
 }
